92e259394fa195247aeda7f2ada2f2534ae2ba78
yankiweiss <yakovw2706@gmail.com>
2025-11-11 09:55:50 -0500

    update

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+require('dotenv').config()
+
+const port = 4000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/email', (req, res) => {
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+let mailOptions = {
+  from: process.env.EMAIL,
+  to: 'yakovw2706@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    res.send(200)
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+})
+
+app.listen(port, ()=> {
+    console.log(`Server ruining on ${port}`)
+})
